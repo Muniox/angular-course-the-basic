3c8ded8bc45c902d96cde94394783bf39dc1a2bc
Muniox <muniox20@gmail.com>
2024-04-05 10:26:47 +0200

    Getting the index when using ngFor

--- src/app/tasks/practising-directives/practising-directives.component.ts
+++ src/app/tasks/practising-directives/practising-directives.component.ts
@@ -1,23 +1,27 @@
 import { Component } from '@angular/core';
 
 interface Log {
-  id: number
-  timestamp: number
+  id: number;
+  timestamp: string;
 }
 
 @Component({
   selector: 'app-practising-directives',
   templateUrl: './practising-directives.component.html',
-  styleUrl: './practising-directives.component.css'
+  styleUrl: './practising-directives.component.css',
 })
 export class PractisingDirectivesComponent {
-  logs:Log[] = [];
-  date = new Date()
-  hours = this.date.getHours()
-  minutes = this.date.getMinutes()
+  logs: Log[] = [];
+  paragraphToggle = true;
 
-  logButtonClick() {
-    this.logs.push({id: this.logs.length + 1, timestamp: new Date().})
+  onLogButtonClick() {
+    const date = new Date();
+
+    this.logs.push({
+      id: this.logs.length + 1,
+      timestamp: `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`,
+    });
+
+    this.paragraphToggle = !this.paragraphToggle;
   }
-
 }
